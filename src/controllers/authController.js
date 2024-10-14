28fb65dc87249d330d01d2caaddf0d92bac3a09a
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2024-10-14 17:17:13 +0530

    changed bycrypt library here

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 // playerController.js
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Candidate = require("../models/candidateModel.js");
 
